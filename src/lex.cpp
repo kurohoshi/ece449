94998753b3406246df240ee65b11c9490ad6121e
LingXing <lzheng17@hawk.iit.edu>
2017-09-03 12:37:59 -0500

    added number token functionality

--- src/lex.cpp
+++ src/lex.cpp
@@ -4,12 +4,15 @@
 
 int main(int argc, char *argv[])
 {
+    //throw error if no file is provided for lex analysis
     if (argc < 2)
     {
         std::cerr << "You should provide a file name." << std::endl;
         return -1;
     }
 
+    //initialize input file to be read
+    //throw error if input file cannot be read
     std::ifstream input_file(argv[1]);
     if (!input_file)
     {
@@ -17,17 +20,21 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    //name output file as input file + tokens
     std::string output_file_name = std::string(argv[1])+".tokens";
     std::ofstream output_file(output_file_name);
+    //throw error if output file cannot be written
     if (!output_file)
     {
         std::cerr << "I can't write " << argv[1] << ".tokens ." << std::endl;
         return -1;
     }
 
+    //iterate thru file for each line starting fromn first line
     std::string line;
     for (int line_no = 1; std::getline(input_file, line); ++line_no)
     {
+        //iterate through each character of the line
         for (size_t i = 0; i < line.size();)
         {
             // comments
@@ -81,6 +88,37 @@ int main(int argc, char *argv[])
                 output_file << "NAME "
                     << line.substr(name_begin, i-name_begin) << std::endl;
             }
+
+            //NUMBER
+            if((line[i] >= '0') && (line[i] <= '9'))
+            {
+                size_t num_begin = i;
+                for(++i; i < line.size(); ++i)
+                {
+                    if((line[i] == ' ') || (line[i] == '\t')
+                       || (line[i] == '\r') || (line[i] == '\n')
+                       || (line[i] == '(') || (line[i] == ')')
+                       || (line[i] == '[') || (line[i] == ']')
+                       || (line[i] == ':') || (line[i] == ';')
+                       || (line[i] == ','))
+                    {
+                        break; //break if whitespace or single token type encountered 
+                    }
+
+                    //throw error if not whitespace, singles, nor number
+                    if(!((line[i] >= '0') && (line[i] <= '9')))
+                    {
+                        //throw error for not a number
+                        std::cerr << "LINE " << line_no
+                            << ": INVALID NUMBER" << std::end1;
+                        return -1;
+                    }
+                    continue; //continue to check next character in number string
+                }
+                output_file << "NUMBER "
+                    << line.substr(num_begin, i-num_begin) << std::end1;
+            }
+
             else
             {
                 std::cerr << "LINE " << line_no
