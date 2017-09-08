8f02ebdbfe9d5e343f1130572f5f78a694011093
LingXing <lzheng17@hawk.iit.edu>
2017-09-07 20:14:37 -0500

    implemented range_check and char_check functions

--- src/lex.cpp
+++ src/lex.cpp
@@ -6,7 +6,7 @@ struct tok{
     enum token_type {NAME, NUMBER, SINGLE};
 
     token_type type;
-    std:string str;
+    std::string str;
 
     int line_no;
 }; //struct tok
@@ -20,6 +20,30 @@ int char_check(char chr, const std::string& str){ //check matching char
     return 0; //return false if no match
 }
 
+int range_check(char chr, int mode){ //check 0-9(1) or a-z(2) or A-Z(3)
+    switch(mode){
+        default:
+            std::cerr << "invalid mode" << std::endl;
+            return 0;
+        case 1:
+            if((chr >= '0') && (chr <= '9')){
+                return 1;
+            }
+            break;
+        case 2:
+            if((chr >= 'a') && (chr <= 'z')){
+                return 1;
+            }
+            break;
+        case 3:
+            if((chr >= 'A') && (chr <= 'Z')){
+                return 1;
+            }
+            break;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[]){
     //throw error if no file is provided for lex analysis
     if (argc < 2){
@@ -31,7 +55,7 @@ int main(int argc, char *argv[]){
     //throw error if input file cannot be read
     std::ifstream input_file(argv[1]);
     if (!input_file){
-        std::cerr << "I can't read " << argv[1] << "." << std::endl;
+        std::cerr << "I can't read" << argv[1] << "." << std::endl;
         return -1;
     }
 
@@ -49,14 +73,11 @@ int main(int argc, char *argv[]){
     for (int line_no = 1; std::getline(input_file, line); ++line_no){
         //iterate through each character of the line
         for (size_t i = 0; i < line.size();){
-            //print current character to be processed
-            //std::cout << line[i] << std::endl;
 
-            //std::cout << "checking comments" << std::endl;
             // comments
-            if (line[i] == '/'){
+            if (char_check(line[i], "/")){
                 ++i;
-                if ((i == line.size()) || (line[i] != '/')){
+                if ((i == line.size()) || char_check(line[i], "/")){
                     std::cerr << "LINE " << line_no
                         << ": a single / is not allowed" << std::endl;
                     return -1;
@@ -64,36 +85,29 @@ int main(int argc, char *argv[]){
                 break; // skip the rest of the line by exiting the loop
             }
 
-            //std::cout << "checking whitespaces" << std::endl;
-            // spaces
-            if ((line[i] == ' ') || (line[i] == '\t')
-                || (line[i] == '\r') || (line[i] == '\n')){
+            //spaces
+            if (char_check(line[i], " \t\r\n")){
                 ++i; // skip this space character
                 continue; // skip the rest of the iteration
             }
 
-            //std::cout << "checking singles" << std::endl;
             // SINGLE
-            if ((line[i] == '(') || (line[i] == ')')
-                || (line[i] == '[') || (line[i] == ']')
-                || (line[i] == ':') || (line[i] == ';')
-                || (line[i] == ',')){
+            if (char_check(line[i], "()[]:;,")){
                 output_file << "SINGLE " << line[i] << std::endl;
                 ++i; // we consumed this character
                 continue; // skip the rest of the iteration
             }
 
-            //std::cout << "checking name" << std::endl;
             // NAME
-            if (((line[i] >= 'a') && (line[i] <= 'z'))       // a to z
-                || ((line[i] >= 'A') && (line[i] <= 'Z'))    // A to Z
-                || (line[i] == '_')){
+            if (range_check(line[i], 2) ||
+                range_check(line[i], 3) ||
+                char_check(line[i], "_")){
                 size_t name_begin = i;
                 for (++i; i < line.size(); ++i){
-                    if (!(((line[i] >= 'a') && (line[i] <= 'z'))
-                        || ((line[i] >= 'A') && (line[i] <= 'Z'))
-                        || ((line[i] >= '0') && (line[i] <= '9'))
-                        || (line[i] == '_') || (line[i] == '$'))){
+                    if (!(range_check(line[i], 1) ||
+                        range_check(line[i], 2) ||
+                        range_check(line[i], 3) ||
+                        char_check(line[i], "_$"))){
                         break; // [name_begin, i) is the range for the token
                     }
                 }
@@ -102,22 +116,16 @@ int main(int argc, char *argv[]){
                 continue;
             }
 
-            //std::cout << "checking number" << std::endl;
             //NUMBER
-            if((line[i] >= '0') && (line[i] <= '9')){
+            if(range_check(line[i], 1)){
                 size_t num_begin = i;
                 for(++i; i < line.size(); ++i){
-                    if((line[i] == ' ') || (line[i] == '\t')
-                       || (line[i] == '\r') || (line[i] == '\n')
-                       || (line[i] == '(') || (line[i] == ')')
-                       || (line[i] == '[') || (line[i] == ']')
-                       || (line[i] == ':') || (line[i] == ';')
-                       || (line[i] == ',')){
-                        break; //break if whitespace or single token type encountered
+                    if(char_check(line[i], " \t\r\n()[]:;,")){
+                        break; //break if whitespace/single token type encountered
                     }
 
                     //throw error if not whitespace, singles, nor number
-                    if(!((line[i] >= '0') && (line[i] <= '9'))){
+                    if(!(range_check(line[i], 1))){
                         //throw error for not a number
                         std::cerr << "LINE " << line_no
                             << ": INVALID NUMBER" << std::endl;
