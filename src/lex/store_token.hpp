c78f2b0b9897637f3ffd40532ce12c4400c4e52a
Ling Xing Zheng <xingling39@gmail.com>
2017-09-10 17:29:15 -0500

    more project reorganization

--- src/lex/store_token.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef STORE_TOKENS
-#define STORE_TOKENS
-
-bool store_tokens_to_file(std::string file_name,
-    const std::vector<evl_token> &tokens);
-
-#endif
--- src/lex/token_gen.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef TOKEN_GEN
-#define TOKEN_GEN
-
-bool extract_tokens_from_file(std::string file_name,
-    std::vector<evl_token> &tokens);
-
-#endif
