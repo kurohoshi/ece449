5734f4b919d2b6292112029092d9caad97d80820
LingXing <lzheng17@hawk.iit.edu>
2017-09-12 23:46:32 -0500

    fixed include mistakes

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 export PATH=/home/ece449/gcc-5.4.0/bin:$PATH
 export LD_LIBRARY_PATH=/home/ece449/gcc-5.4.0/lib64:$LD_LIBRARY_PATH
 
-g++ src/*.cpp -Wall -std=c++11 -g -lm -Wl,-rpath=/home/ece449/gcc-5.4.0/lib64 -o myevl
+g++ src/*.h src/*.cpp  -Wall -std=c++11 -g -lm -Wl,-rpath=/home/ece449/gcc-5.4.0/lib64 -o myevl
--- src/token_gen.h
+++ src/token_gen.h
@@ -1,6 +1,11 @@
 #ifndef TOKEN_GEN
 #define TOKEN_GEN
 
+#include <string>
+#include <vector>
+
+#include "evl_token.h"
+
 bool extract_tokens_from_file(std::string file_name,
     std::vector<evl_token> &tokens);
 
