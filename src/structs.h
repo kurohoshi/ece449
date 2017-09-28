b0a99622d143d4260219f545ecd1074e0835023e
LingXing <lzheng17@hawk.iit.edu>
2017-09-28 16:09:28 -0500

    changed structs to classes and added components and module class

--- src/structs.h
+++ src/structs.h
@@ -4,31 +4,64 @@
 #include <string>
 #include <list>
 
-struct evl_token {
+class evl_token {
+public:
     enum token_type {NAME, NUMBER, SINGLE};
 
     token_type type;
     std::string str;
-
     int line_no;
-}; //struct evl_token
+//public:
+//    evl_token(token_type type, std::string str, int line_no);
+    
+}; // evl_token class
 
 typedef std::list<evl_token> evl_tokens;
 
-struct evl_statement {
+class evl_statement {
+public:
     enum statement_type {MODULE, WIRE, COMPONENT, ENDMODULE};
 
     statement_type type;
     evl_tokens tokens;
-}; // struct evl_statement
+//public:
+//    evl_statement(statement_type type, evl_tokens tokens);
+}; // evl_statement class
 
 typedef std::list<evl_statement> evl_statements;
 
-struct evl_wire {
+class evl_wire {
+public:
     std::string name;
     int width;
-}; // struct evl_wire
+//public:
+//    evl_wire(std::string name, int width);
+}; // evl_wire class
 
 typedef std::list<evl_wire> evl_wires;
+
+class evl_component {
+public:
+    std::string type;
+    std::string name;
+private:
+    class evl_pin {
+        std::string name;
+        int bus_msb;
+        int bus_lsb;
+    }; // pin class
+public:
+    evl_pin pin;
+}; // component class
+
+typedef std::list<evl_component> evl_components;
+
+class evl_module {
+public:
+    evl_wires wires;
+    evl_components components;
+}; // a module class
+
+typedef std::list<evl_module> evl_modules;
 
 #endif
