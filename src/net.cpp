252eddac726010f1e1586f7ed6770918ae97f9ac
Ling Xing Zheng <xingling39@gmail.com>
2017-10-18 10:39:31 -0500

    still fixing things

--- src/net.cpp
+++ src/net.cpp
@@ -13,7 +13,7 @@
 //             Net Member Functions
 //********************************************************
 
-bool net::append_pin(pin *p) {
+void net::append_pin(pin *p) {
     connections_.push_back(p);
 }
 
@@ -33,6 +33,7 @@ bool pin::create(
     //  net_ = find net_name in nets_table
     //  net_->append_pin(pin);
     //}
+    return true;
 }
 
 //********************************************************
@@ -71,50 +72,11 @@ bool gate::create_pin(
 //             Netlist Member Functions
 //********************************************************
 
-bool netlist::create_net(std::string net_name) {
+void netlist::create_net(std::string net_name) {
     assert(nets_table_.find(net_name) == nets_table_.end());
     net *n = new net(net_name);
     nets_table_[net_name] = n;
     nets_.push_back(n);
-}
-
-std::string netlist::make_net_name(std::string wire_name, int i) {
-    assert(i>= 0);
-    std::ostringstream oss;
-    oss << wire_name << "[" << i << "]";
-    return oss.str();
-}
-
-bool netlist::create_nets(const evl_wires &wires) {
-    for(evl_wires::const_iterator wire = wires.begin(); wire != wires.end(); ++wire) {
-        if(wire.width == 1) {
-            create_net(wire.name);
-        } else {
-            for(int i = 0; i < wire.width; ++i) {
-                create_net(make_net_name(wire.name, i));
-            }
-        }
-    }
-    return true;
-}
-
-bool netlist::create_gate(
-    const evl_component &c,
-    const evl_wires_table &wires_table) {
-
-    gate *g = new gate;
-    gates_.push_back(g);
-    return g->create(c, nets_table_, wires_table);
-}
-
-bool netlist::create_gates(
-    const evl_components &comps,
-    const evl_wires_table &wires_table) {
-
-    for(evl_components::const_iterator c = comps.begin(); c != comps.end(); ++c) {
-        create_gate(c, wires_table);
-    }
-    return true;
 }
 
 bool netlist::create(
@@ -122,8 +84,28 @@ bool netlist::create(
     const evl_components &comps,
     const evl_wires_table &wires_table) {
 
-    return create_nets()
-        && create_gates();
+    for(evl_wires::const_iterator wire = wires.begin(); wire != wires.end(); ++wire) {
+        if(wire.width == 1) {
+            if(create_net(wire.name))
+                return false;
+        } else {
+            for(int i = 0; i < wire.width; ++i) {
+                std::ostringstream oss;
+                oss << wire.name << "[" << i << "]";
+                if(!create_net(oss.str()))
+                    return false;
+            }
+        }
+    }
+
+    for(evl_components::const_iterator c = comps.begin(); c != comps.end(); ++c) {
+        gate *g = new gate;
+        gates_.push_back(g);
+        if(!(g->create(c, nets_table_, wires_table)))
+            return false;
+    }
+
+    return true;
 }
 
 void netlist::display(
